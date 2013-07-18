01b38eb57ee5ce6e8dffaaad5b18e5166a78531f
kubo0180 <gibson02122569@gmail.com>
2013-07-18 17:55:17 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UxdIa::Application.config.secret_token = '7d0b76c3cd0e3732192a68b90b0fc1b72629bb630ac0ad3326cb337b1a2fdf76a27e2550fcc3ebd1168cacfc6e2ce119e4e919818cae7644e180ef530e342390'
